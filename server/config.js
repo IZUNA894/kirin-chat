219c42665554b1d4fb01c5816ef154ffccc673c6
IZUNA894 <jainsanyam1261@gmail.com>
2021-10-24 20:18:28 +0530

    cred removed

--- server/config.js
+++ server/config.js
@@ -1,15 +1,12 @@
 module.exports = {
   MONGO_URL:
-    "mongodb+srv://TONY:SAMNAS2401@cluster1.tuia5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://<username>:<password>@cluster1.tuia5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   MONGO_DATABASE_NAME_TEST: "kirin-test",
   AWS_BUCKET: "kirin-izuna",
   AWS_REGION: "ap-south-1",
-  AWS_USERNAME: "izuna",
-  AWS_ACCESS_KEY_ID: "AKIA35BORXI2JHFNC3SU",
-  AWS_SECRET_ACCESS_KEY: "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"
-  //   ACCESS_KEY_ID: "AKIAR4PFRBJGKIEV6KFS",
-  //   SECRET_ACCESS_KEY: "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",
-  //   AWS_REGION: "us-west-2",
+  AWS_USERNAME: "",
+  AWS_ACCESS_KEY_ID: "",
+  AWS_SECRET_ACCESS_KEY: ""
 };
 
 module.exports.imageUploadFilter = (request, file, cb) => {
@@ -18,11 +15,6 @@ module.exports.imageUploadFilter = (request, file, cb) => {
   }
   cb(null, true);
 };
-
-// AWS_REGION: "ap-south-1",
-//   AWS_USERNAME: "ahnglobal",
-//   AWS_ACCESS_KEY_ID: "AKIAZQGPBJIZLV4F47YZ",
-//   AWS_SECRET_ACCESS_KEY: "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",
 
 module.exports.getOTPForChurchSignUp = () => {
   let minm = 10000;
