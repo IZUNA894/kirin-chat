e2de5fa19edf64c8912cc9989617407a2603df03
IZUNA894 <jainsanyam1261@gmail.com>
2021-10-24 19:46:47 +0530

    final commit

--- server/utils/mailer/mailer.js
+++ server/utils/mailer/mailer.js
@@ -3,17 +3,17 @@ const mailer = require("nodemailer");
 const SES = require("../aws/ses").defaultSES;
 const transporter = mailer.createTransport({ SES: SES() });
 
-const {
-  verifySignupEmail: verifyOrganizationEmail
-} = require("./organizations/organization");
+// const {
+//   verifySignupEmail: verifyOrganizationEmail
+// } = require("./organizations/organization");
 const {
   verifySignupEmail: verifyUserEmail,
   forgotPasswordEmail
 } = require("./users/user");
 // const { forgotPassword } = require("./forgot-password/forgot-password");
 // const { forgotUserPassword } = require("./forgot-password/forgot-password");
-const { sendProspectContract } = require("./contract/prospect");
-const { sendClientContract } = require("./contract/client");
+// const { sendProspectContract } = require("./contract/prospect");
+// const { sendClientContract } = require("./contract/client");
 // const SmartChalkMail = require("./smartchalk/smart-chalk");
 // const AfterSalesMail = require("./after-sales/after-sales");
 // const SmartChalkIntro = require("./smartchalk-intro/smartchalk-intro");
@@ -43,23 +43,23 @@ module.exports.sendUserSignUpVerificationMail = parameters => {
     });
 };
 
-module.exports.sendOrganizationForgotPasswordMail = parameters => {
-  const mailOptions = forgotPassword(parameters);
-  return transporter
-    .sendMail(mailOptions)
-    .then(result => {
-      return {
-        status: true,
-        message:
-          "You will recieve an email with reset instructions if you are registerted with us"
-      };
-    })
-    .catch(error => {
-      throw new Error(
-        `Error sending email please contact support ${error.message}`
-      );
-    });
-};
+// module.exports.sendOrganizationForgotPasswordMail = parameters => {
+//   const mailOptions = forgotPassword(parameters);
+//   return transporter
+//     .sendMail(mailOptions)
+//     .then(result => {
+//       return {
+//         status: true,
+//         message:
+//           "You will recieve an email with reset instructions if you are registerted with us"
+//       };
+//     })
+//     .catch(error => {
+//       throw new Error(
+//         `Error sending email please contact support ${error.message}`
+//       );
+//     });
+// };
 
 module.exports.sendUserForgotPasswordMail = parameters => {
   const mailOptions = forgotPasswordEmail(parameters);
@@ -79,12 +79,12 @@ module.exports.sendUserForgotPasswordMail = parameters => {
     });
 };
 
-module.exports.sendProspectContractMail = parameters => {
-  const mailOptions = sendProspectContract(parameters);
-  return transporter.sendMail(mailOptions);
-};
+// module.exports.sendProspectContractMail = parameters => {
+//   const mailOptions = sendProspectContract(parameters);
+//   return transporter.sendMail(mailOptions);
+// };
 
-module.exports.sendClientContractMail = parameters => {
-  const mailOptions = sendClientContract(parameters);
-  return transporter.sendMail(mailOptions);
-};
+// module.exports.sendClientContractMail = parameters => {
+//   const mailOptions = sendClientContract(parameters);
+//   return transporter.sendMail(mailOptions);
+// };
